d4b3925901c1e0014e291f12cb2a7c8a476778b8
MrNewb <47620135+MrNewb@users.noreply.github.com>
2026-09-02 03:23:39 -0400

    beekeeping

--- pages/mrnewbbeekeeping/install/_meta.jsx
+++ pages/mrnewbbeekeeping/install/_meta.jsx
@@ -1,8 +1,9 @@
-import { IconPackage, IconPhoto, IconTerminal2 } from '@tabler/icons-react'
+import { IconPackage, IconPhoto, IconSettings, IconTerminal2 } from '@tabler/icons-react'
 import { sidebarTitle } from '../../../components/SidebarLabel'
 
 export default {
   commands: sidebarTitle(IconTerminal2, 'Commands'),
+  configuration: sidebarTitle(IconSettings, 'Configuration'),
   inventory: sidebarTitle(IconPackage, 'Item setup'),
   'inventory-images': sidebarTitle(IconPhoto, 'Item images'),
 }
